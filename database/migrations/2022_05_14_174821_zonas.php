70ccc3acc5f4ac11a617074fb875c21a5e2d7d5b
SofiRaciti8 <sofiracitiii@gmail.com>
2022-05-14 15:16:40 -0300

    modelos, tablas y vistas

---
+++ database/migrations/2022_05_14_174821_zonas.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Zonas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('zonas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombreZona');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_05_14_175144_turnos.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Turnos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('turnos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_vacuna');
+            $table->unsignedBigInteger('id_zona');
+            $table->unsignedBigInteger('id_paciente');
+            $table->date("fecha");
+            $table->date("hora");
+            $table->enum("estado", ["aplicado", "cancelado", "ausente","pendiente"])->default("pendiente");
+            $table->foreign("id_vacuna")
+                ->references("id")
+                ->on("vacunas")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
+            $table->foreign("id_zona")
+                ->references("id")
+                ->on("zonas")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
+            $table->foreign("id_paciente")
+                ->references("id")
+                ->on("users")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
