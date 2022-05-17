6dba03a60cb64e09882736c0bc75ee007995bd2d
santiago <santiiagodossantos@icloud.com>
2022-05-16 22:04:35 -0300

    Fix JS Left Sidebar Menu

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -2,7 +2,7 @@
         <!-- ============================================================== -->
         <div class="nav-left-sidebar sidebar-dark">
             <div class="menu-list">
-                <nav class="navbar navbar-expand-lg navbar-light">
+                <nav class="navbar navbar-expand navbar-light">
                     <a class="d-xl-none d-lg-none" href="#">Dashboard</a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -288,3 +288,5 @@
                 </nav>
             </div>
         </div>
+
+        @include('partials.menu-scripts');
