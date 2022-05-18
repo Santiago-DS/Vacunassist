d101cd2397235092ed02e869228ebbc46f889f0c
SofiRaciti8 <sofiracitiii@gmail.com>
2022-05-18 20:00:00 -0300

    estilos certificado

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -1,8 +1,94 @@
 <head>
-   
+  
 </head>
+<style>
+  table {
+    table-layout: fixed;
+    width: 65%;
+    border-collapse: collapse;
+    border: 3px solid rgb(126, 203, 223);
+    margin-left: 15%;
+}
+thead th:nth-child(1) {
+  width: 30%;
+}
 
-<table class="table table-bordered">
+thead th:nth-child(2) {
+  width: 20%;
+}
+
+thead th:nth-child(3) {
+  width: 15%;
+}
+
+thead th:nth-child(4) {
+  width: 35%;
+}
+
+th, td {
+  padding: 20px;
+}
+
+html {
+  font-family: 'helvetica neue', helvetica, arial, sans-serif;
+}
+
+thead th, tfoot th {
+  font-family: 'Rock Salt', cursive;
+}
+
+th {
+  letter-spacing: 2px;
+}
+
+td {
+  letter-spacing: 1px;
+}
+
+tbody td {
+  text-align: center;
+}
+
+tfoot th {
+  text-align: right;
+}
+thead, tfoot {
+  background: url(leopardskin.jpg);
+  color: white;
+  text-shadow: 1px 1px 1px black;
+}
+
+thead th, tfoot th, tfoot td {
+  background: linear-gradient(to bottom, rgba(0,0,0,0.1), rgba(0,0,0,0.5));
+  border: 3px solid rgb(135, 235, 230);
+}
+tbody tr:nth-child(odd) {
+  background-color: #e1cddc;
+}
+
+tbody tr:nth-child(even) {
+  background-color: #a9e7f0;
+}
+
+tbody tr {
+  background-image: url(noise.png);
+}
+
+table {
+  background-color: #bb9db3;
+}
+caption {
+  font-family: 'Rock Salt', cursive;
+  padding: 20px;
+  font-style: italic;
+  caption-side: bottom;
+  color: #666;
+  text-align: right;
+  letter-spacing: 1px;
+}
+  </style>
+<body>   
+<table>
     <thead>
       <tr>
         <th scope="col">Nombre</th>
@@ -28,7 +114,8 @@
     ?>
 
   </table> 
-
+  
   
   <p>Certificado Para: <?php echo auth()->user()->name . " " . auth()->user()->apellido ?><p>
     <p>Fecha de Emisión: <?php echo $result ?><p>
+    </body>
