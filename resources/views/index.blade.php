cf1883fa1ba666e1f7aacd97b16a10c2ea9777d2
santiago <santiiagodossantos@icloud.com>
2022-05-18 13:07:07 -0300

    Index Nuevos Colores

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,53 +7,18 @@
   <meta name="copyright" content="MACode ID, https://macodeid.com/">
   <title>Vacunassist</title>
 </head>
-
-@include('partials.nav')
-
+  @include('partials.nav')
 <body>
 
-  <div class="page-hero bg-image overlay-dark" style="background-image: url(../assets/img/bg_image_1.jpg);"></div>
-  <div class="bg-light">
-    <div class="page-section py-3 mt-md-n5 custom-index">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-md-4 py-3 py-md-0">
-            <div class="card-service wow fadeInUp">
-              <div class="circle-shape bg-secondary text-white">
-                <span class="mai-chatbubbles-outline"></span>
-              </div>
-              <p><span>Chat</span> with a doctors</p>
-            </div>
-            
-          </div>
-          <div class="col-md-4 py-3 py-md-0">
-            <div class="card-service wow fadeInUp">
-              <div class="circle-shape bg-secondary text-white">
-                <span class="mai-chatbubbles-outline"></span>
-              </div>
-              <p><span>Chat</span> with a doctors</p>
-            </div>
-          </div>
-          <div class="col-md-4 py-3 py-md-0">
-            <div class="card-service wow fadeInUp">
-              <div class="circle-shape bg-accent text-white">
-                <span class="mai-basket"></span>
-              </div>
-              <p><span>One</span>-Health Pharmacy</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> <!-- .page-section -->
 
-    <div class="page-section pb-0">
+  <div style="background-color: rgb(238, 232, 232)">
+    <div class="page-section pb-0" style="background-color: rgb(238, 232, 232)">
       <div class="container">
         <div class="row align-items-center">
           <div class="col-lg-6 py-3 wow fadeInUp">
             <h1>¡Bienvenido a Vacunassist!</h1>
-            <p class="text-grey mb-4">Gestiona tus turnos y mucho mas...</p>
-            <img src="../assets/img/logo.png" width="300 " height="185" />
-
+            <p class="text-grey mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores quasi reiciendis debitis explicabo necessitatibus minus laborum asperiores ducimus tempore minima nemo natus vel ad, fugiat autem quibusdam a libero obcaecati.</p>
+            <img src="../assets/img/logo.png" width="300 " height="185"/>
           </div>
           <div class="col-lg-6 wow fadeInRight" data-wow-delay="400ms">
             <div class="img-place custom-img-1">
@@ -62,26 +27,9 @@
           </div>
         </div>
       </div>
-    </div> <!-- .bg-light -->
-  </div> <!-- .bg-light -->
+    </div> 
+  </div>
   
-
-
-  <div class="page-section banner-home bg-image" style="background-image: url(../assets/img/banner-pattern.svg);">
-    <div class="container py-5 py-lg-0">
-      <div class="row align-items-center">
-        <div class="col-lg-4 wow zoomIn">
-          <div class="img-banner d-none d-lg-block">
-            <img src="../assets/img/mobile_app.png" alt="">
-          </div>
-        </div>
-        <div class="col-lg-8 wow fadeInRight">
-          <h1 class="font-weight-normal mb-3">Registrate para acceder a todos los beneficios de Vacunassist</h1>
-        </div>
-      </div>
-    </div>
-  </div> <!-- .banner-home -->
-
   <footer class="page-footer">
     <div class="container">
       <div class="row px-md-3">
@@ -115,34 +63,16 @@
           <h5>Contact</h5>
           <p class="footer-link mt-2">351 Willow Street Franklin, MA 02038</p>
           <a href="#" class="footer-link">701-573-7582</a>
-          <a href="#" class="footer-link">healthcare@temporary.net</a>
-
-          <h5 class="mt-3">Social Media</h5>
-          <div class="footer-sosmed mt-3">
-            <a href="#" target="_blank"><span class="mai-logo-facebook-f"></span></a>
-            <a href="#" target="_blank"><span class="mai-logo-twitter"></span></a>
-            <a href="#" target="_blank"><span class="mai-logo-google-plus-g"></span></a>
-            <a href="#" target="_blank"><span class="mai-logo-instagram"></span></a>
-            <a href="#" target="_blank"><span class="mai-logo-linkedin"></span></a>
-          </div>
+          <a href="#" class="footer-link">healthcare@temporary.net</a>     
         </div>
       </div>
-
       <hr>
-
       <p id="copyright">Copyright &copy; 2020 <a href="https://macodeid.com/" target="_blank">MACode ID</a>. All right reserved</p>
     </div>
   </footer>
 
-<script src="../assets/js/jquery-3.5.1.min.js"></script>
 
-<script src="../assets/js/bootstrap.bundle.min.js"></script>
-
-<script src="../assets/vendor/owl-carousel/js/owl.carousel.min.js"></script>
-
-<script src="../assets/vendor/wow/wow.min.js"></script>
-
-<script src="../assets/js/theme.js"></script>
   
 </body>
 </html>
+
