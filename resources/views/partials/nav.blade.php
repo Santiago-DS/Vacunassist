843df5d67a077368475ca15c2542980dde471551
SofiRaciti8 <sofiracitiii@gmail.com>
2022-05-21 12:41:49 -0300

    como estaba

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -7,20 +7,6 @@
     <link rel="stylesheet" href="../assets/css/bootstrap.css">
     <link rel="stylesheet" href="../assets/css/theme.css">
 </head>
-<style>
-  .enlace{
-    box-shadow: 5px 5px 5px black;
-    padding: 10px;
-    border-radius: 5px;
-    text-decoration: none;
-    background: rgb(224, 229, 233);
-    color:black;
-    margin: 10px;
-  }
-  .enlace:hover{
-    background: rgb(176, 253, 230);
-  }
-  </style>
 <header>
 
     <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
@@ -33,7 +19,7 @@
           <ul class="navbar-nav ml-auto">
             @auth
             <li class="nav-item active">
-              <a class="enlace" href="/home">Home</a>
+              <a class="btn btn-primary ml-lg-3" href="/home">Home</a>
             </li>
             @else
             <li class="nav-item">
@@ -46,7 +32,7 @@
             @auth
             <form action="/logout" method="POST">
                 @csrf
-                <a class="enlace" href="#" onclick="this.closest('form').submit()">Cerrar Sesion</a>
+                <a class="btn btn-danger ml-lg-3" href="#" onclick="this.closest('form').submit()">Cerrar Sesion</a>
             </form>
             @endauth
           </ul>
