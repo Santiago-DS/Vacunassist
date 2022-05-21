c60098e8d756120f4b08c14dd42052270cbc6aa8
Santiago <santiago@Santiagos-iMac.local>
2022-05-20 23:54:00 -0300

    Values old registro

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -11,14 +11,14 @@
                   <div class="row justify-content-center">
                     <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                       <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Registrate</p>
-      
+
                     <form action="{{ route('register') }}" class="mx-1 mx-md-4" method="POST">
                         @csrf
                         <div class="d-flex flex-row align-items-center mb-2">
                           <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                           <div class="form-outline flex-fill mb-0">
                             <label class="form-label">Nombre</label>
-                            <input type="text" name='nombre' class="form-control"/>
+                            <input type="text" name='nombre' class="form-control" value="{{ old('nombre') }}"/>
                             @error('nombre') <small style="color: red" >*{{ $message }}</small> @enderror
                           </div>
                         </div>
@@ -27,7 +27,7 @@
                             <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                             <div class="form-outline flex-fill mb-0">
                                 <label class="form-label">Apellido</label>
-                                <input type="text" name='apellido' class="form-control"/>
+                                <input type="text" name='apellido' class="form-control" value="{{ old('apellido') }}"/>
                                 @error('apellido') <small style="color: red" >*{{ $message }}</small> @enderror
 
                             </div>
@@ -37,9 +37,9 @@
                             <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                             <div class="form-outline flex-fill mb-0">
                               <label class="form-label">Correo</label>
-                              <input type="email" name="email" class="form-control"/> 
+                              <input type="email" name="email" class="form-control" value="{{ old('email') }}"/>
                               @error('email') <small style="color: red" >*{{ $message }}</small> @enderror
-   
+
                             </div>
                         </div>
 
@@ -47,7 +47,7 @@
                             <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                             <div class="form-outline flex-fill mb-0">
                               <label class="form-label">Dirección</label>
-                              <input type="text" name='direccion' class="form-control"/>
+                              <input type="text" name='direccion' class="form-control" value="{{ old('direccion') }}"/>
                               @error('direccion') <small style="color: red" >*{{ $message }}</small> @enderror
 
                             </div>
@@ -57,9 +57,9 @@
                             <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                             <div class="form-outline flex-fill mb-0">
                               <label class="form-label">Teléfono</label>
-                              <input type="text" name='telefono' class="form-control"/>                           
+                              <input type="text" name='telefono' class="form-control" value="{{ old('telefono') }}"/>
                               @error('telefono') <small style="color: red" >*{{ $message }}</small> @enderror
-  
+
                             </div>
                         </div>
 
@@ -67,25 +67,25 @@
                             <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                             <div class="form-outline flex-fill mb-0">
                               <label class="form-label">Contraseña</label>
-                              <input type="password" name='password' class="form-control"/> 
+                              <input type="password" name='password' class="form-control"/>
                               @error('password') <small style="color: red" >*{{ $message }}</small> @enderror
- 
+
                             </div>
                         </div>
                         <div class="d-flex flex-row align-items-center mb-3">
                           <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                           <div class="form-outline flex-fill mb-0">
                             <label class="form-label">Repetir Contraseña</label>
-                            <input name='password-confirm' type="password" class="form-control"/> 
+                            <input name='password-confirm' type="password" class="form-control"/>
                             @error('password-confirm') <small style="color: red" >*{{ $message }}</small> @enderror
- 
+
                           </div>
                       </div>
                         <div class="d-flex flex-row align-items-center mb-4">
                           <button type="submit" class="btn btn-primary btn-lg">Registrarse</button>
                         </div>
                     </div>
-                    
+
                     <div class="col-md-10 col-lg-6 col-xl-7 f-flex align-items-center order-1 order-lg-2">
                         <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" class="img-fluid">
                     <div class="d-flex flex-row align-items-right mb-2">
@@ -95,15 +95,15 @@
                           <i class="fas fa-user fa-lg me-1 fa-fw"></i>
                           <div class="form-outline mb-4" style="margin-right: 70%">
                             <label class="form-label">Fecha de Nacimiento</label>
-                            <input type="date" name='fecha_nacimiento' class="form-control"/> 
+                            <input type="date" name='fecha_nacimiento' class="form-control" value="{{ old('fecha_nacimiento')}}"/>
                             @error('fecha_nacimiento') <small style="color: red" >*{{ $message }}</small> @enderror
- 
+
                         </div>
                         <div class="f-flex align-items-center mb-2">
                           <i class="fas fa-user fa-lg me-1 fa-fw"></i>
                           <div class="form-outline mb-0" style="margin-right: 70%">
                             <label class="form-label">Documento</label>
-                            <input type="text" name='documento' class="form-control"/>  
+                            <input type="text" name='documento' class="form-control" value="{{ old('documento')}}"/>
                             @error('documento') <small style="color: red" >*{{ $message }}</small> @enderror
 
                           </div>
@@ -112,13 +112,13 @@
                             <label>¿Cúantas dosis tenés de la vacuna contra el COVID?</label><br><br>
                             <input type="checkbox" id="cbox1" value="first_checkbox"> Ningna</label><br><br>
                             <input type="checkbox" id="cbox1" value="first_checkbox"> 1 Dosis</label><br><br>
-                            <input type="checkbox" id="cbox1" value="first_checkbox"> 2 Dosis</label><br><br>   
-                          </div> 
-                        </div>             
+                            <input type="checkbox" id="cbox1" value="first_checkbox"> 2 Dosis</label><br><br>
+                          </div>
+                        </div>
                       </div>
-                            
+
                     </div>
-                        
+
                     </div>
                 </div>
               </div>
