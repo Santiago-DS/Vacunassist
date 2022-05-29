cf7aae0009a02bf938bd343612150f2cf9378c60
SofiRaciti8 <sofiracitiii@gmail.com>
2022-05-29 12:15:36 -0300

    home enfemero

--- resources/views/homeEnfermero.blade.php
+++ resources/views/homeEnfermero.blade.php
@@ -1,106 +1,181 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
-    <link href="assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/libs/css/style.css">
-    <link rel="stylesheet" href="assets/vendor/fonts/fontawesome/css/fontawesome-all.css">
-    <link rel="stylesheet" href="assets/vendor/charts/chartist-bundle/chartist.css">
-    <link rel="stylesheet" href="assets/vendor/charts/morris-bundle/morris.css">
-    <link rel="stylesheet" href="assets/vendor/fonts/material-design-iconic-font/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="assets/vendor/charts/c3charts/c3.css">
-    <link rel="stylesheet" href="assets/vendor/fonts/flag-icon-css/flag-icon.min.css">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Ver Turnos</title>
 
-    
-    <title>Home Enfermero</title>
-</head>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+        <link href="assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/libs/css/style.css">
+        <link rel="stylesheet" href="assets/vendor/fonts/fontawesome/css/fontawesome-all.css">
+        <link rel="stylesheet" href="assets/vendor/charts/chartist-bundle/chartist.css">
+        <link rel="stylesheet" href="assets/vendor/charts/morris-bundle/morris.css">
+        <link rel="stylesheet" href="assets/vendor/fonts/material-design-iconic-font/css/materialdesignicons.min.css">
+        <link rel="stylesheet" href="assets/vendor/charts/c3charts/c3.css">
+        <link rel="stylesheet" href="assets/vendor/fonts/flag-icon-css/flag-icon.min.css">
+    </head>
 
-<body>
-    <!-- ============================================================== -->
-    <!-- main wrapper -->
-    <!-- ============================================================== -->
-    <div class="dashboard-main-wrapper">
+    <body>
 
-        <!-- ============================================================== -->
-        <!-- navbar -->
-        <!-- ============================================================== -->
 
-        <div class="dashboard-header">
-            <nav class="fixed-top">
-                @include('partials.nav')
-            </nav>
-        </div>
-        <!-- ============================================================== -->
-        <!-- end navbar -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-            @include('partials.menu')
 
-        <!-- ============================================================== -->
-        <!-- end left sidebar -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- wrapper  -->
-        <!-- ============================================================== -->
+        <div class="dashboard-main-wrapper">
+
+            <div class="dashboard-header">
+                <nav class="fixed-top">
+                    @include('partials.nav')
+                </nav>
+            </div>
+
+        @include('partials.menu-enfermero')
+
         <div class="dashboard-wrapper">
             <div class="dashboard-ecommerce">
                 <div class="container-fluid dashboard-content ">
-                    <!-- ============================================================== -->
-                    <!-- pageheader  -->
-                    <!-- ============================================================== -->
                     <div class="row">
                         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                             <div class="page-header">
-                                <h2 class="pageheader-title">Home Enfermero</h2>
+                                <h2 class="pageheader-title">Gestión de Turnos</h2>
                                 <div class="page-breadcrumb">
                                     <nav aria-label="breadcrumb">
                                         <ol class="breadcrumb">
-                                            <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Vacunassist Home</a></li>
+                                            <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Home</a></li>
+                                            <li class="breadcrumb-item active" aria-current="page">Ver Turnos Pendientes</li>
                                         </ol>
                                     </nav>
                                 </div>
                             </div>
                         </div>
-
-                        <img src="../assets/img/imagen_home.png" width="1077" height="443"/>
-
                     </div>
-                    <!-- ============================================================== -->
-                    <!-- end pageheader  -->
-                    <!-- ============================================================== -->
+                    <div class="ecommerce-widget">
 
-                </div>
-                
-            </div>
-        
+                <div class="row">
+        <div class="col-xl-9 col-lg-8 col-md-11 col-sm-12 col-2">
+            <div class="card">
+                <?php $mytime = Carbon\Carbon::now()->format('Y-m-d'); 
+                $mytimev = Carbon\Carbon::now()->format('d/m/Y');
+                ?>
+                <h5 class="card-header">Turnos Pendientes. Fecha: <?php echo $mytimev ?> </h5>
+                <div class="card-body">
+                <div class="container">
+  <div class="row">
+    <div class="col-12">
+        <?php
+            $id_usuario=auth()->id();
+            $turnos = DB::table('turnos')
+            ->select('turnos.id AS id_turno' , 'turnos.id_vacuna' ,
+            'turnos.fecha' , 'turnos.id_zona' , 'vacunas.nombreVacuna' , 'zonas.nombreZona')
+            ->join('vacunas', 'vacunas.id', '=', 'turnos.id_vacuna')
+            ->join('zonas', 'zonas.id', '=', 'turnos.id_zona')
+            ->where('fecha', strval($mytime))
+            ->where('estado' , 'pendiente')
+            ->get();
+        ?>
 
-        </div>
-        <!-- ============================================================== -->
-        <!-- end wrapper  -->
-        <!-- ============================================================== -->
+    @if ($turnos->count())
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">Vacuna</th>
+            <th scope="col">Zona</th>
+            <th scope="col">Estado</th>
+            <th scope="col">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach ($turnos as $turno)
+        <tr>
+            <td>{{ $turno->nombreVacuna}}</td>
+            <td>{{ $turno->nombreZona}}</td>
+            <td><span class="badge-dot badge-success mr-1"></span> Pendiente</td>
+            <td>
+                <form action="{{ route('turnos.edit', ['id'=>$turno->id_turno]) }}" method="get" class="formulario-eliminar">
+                    <button type="submit" class="btn btn-success">
+                    <i class="fas fa-check"></i>Confirmar</button>
+                    <button type="submit" class="btn btn-info">
+                        <i class="fas fa-frown"></i>Ausente</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+        </tbody>
+      </table>
+      @else
+      <td><span class="badge-dot badge-brand mr-1"></span> No hay turnos para mostrar</td>
+      @endif
     </div>
-
-
-    <!-- ============================================================== -->
-    <!-- end main wrapper  -->
-    <!-- ============================================================== -->
-</body>
-
-
-
-@if (session('actualizarContrasenia') == 'ok')
+  </div>
+</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        @include('partials.footer')
+        </div>
+    </body>
+    @if (session('eliminar') == 'ok')
     <script>
         Swal.fire(
-        'Actualizado!',
-        'Su contraseña se modifico con éxito.',
+        'Cancelado!',
+        'El turno se cancelo correctamente.',
+        'success'
+    )
+    </script>
+@endif
+    <script>
+
+        $('.formulario-eliminar').submit(function(e){
+            e.preventDefault();
+            Swal.fire({
+  title: '¿Está seguro de cancelar el turno?',
+  text: "el turno se cancelara definitivamente",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Aceptar',
+  cancelButtonText: 'Cancelar'
+}).then((result) => {
+  if (result.isConfirmed) {
+    this.submit();
+  }
+})
+        });
+    </script>
+@if (session('solicitar') == 'ok')
+    <script>
+        Swal.fire(
+        'Turno exitoso!',
+        'Se generó el turno correctamente.',
+        'success'
+    )
+    </script>
+@endif
+
+@if (session('ninguna') == 'ok')
+    <script>
+        Swal.fire(
+        'Turno exitoso!',
+        'Se generó el turno correctamente para la primer dosis de covid.',
+        'success'
+    )
+    </script>
+@endif
+
+@if (session('una_dosis') == 'ok')
+    <script>
+        Swal.fire(
+        'Turno exitoso!',
+        'Se generó el turno correctamente para la segunda dosis de covid.',
         'success'
     )
     </script>
 @endif
 </html>
-
 
---
+++ resources/views/partials/menu-enfermero.blade.php
@@ -0,0 +1,84 @@
+        <!-- left sidebar -->
+        <!-- ============================================================== -->
+        <div class="nav-left-sidebar sidebar-dark">
+            <div class="menu-list">
+                <nav class="navbar navbar-expand navbar-light">
+                    <a class="d-xl-none d-lg-none" href="#"></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav flex-column" style="border: none">
+                            <li class="nav-divider">
+                                Menu
+                            </li>
+                            <li class="nav-item" href="/miperfil">
+                                <a  href="/miperfil" class="nav-link"><i class="fa fa-fw fa-user-circle"></i>Mi Perfil<span class="badge badge-success">6</span></a>
+                                <div id="submenu-1" class="collapse submenu" style="">
+                                    <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1-2" aria-controls="submenu-1-2">E-Commerce</a>
+                                            <div id="submenu-1-2" class="collapse submenu" style="">
+                                                <ul class="nav flex-column">
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="index.html">E Commerce Dashboard</a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="ecommerce-product.html">Product List</a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="ecommerce-product-single.html">Product Single</a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="ecommerce-product-checkout.html">Product Checkout</a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="dashboard-finance.html">Finance</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="dashboard-sales.html">Sales</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1-1" aria-controls="submenu-1-1">Infulencer</a>
+                                            <div id="submenu-1-1" class="collapse submenu" style="">
+                                                <ul class="nav flex-column">
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="dashboard-influencer.html">Influencer</a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="influencer-finder.html">Influencer Finder</a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="influencer-profile.html">Influencer Profile</a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-5" aria-controls="submenu-5"><i class="fas fa-fw fa-file"></i>Gestion de turnos</a>
+                                <div id="submenu-5" class="collapse submenu" style="">
+                                    <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="historiaclinica">Ver turnos Pendientes</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="formhistoriaclinica">Stock Vacunas</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+
+
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        @include('partials.menu-scripts')
