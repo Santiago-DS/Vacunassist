e010e2255f414e3976ce70cfd1cb855f808a7f34
Santiago <santiago@Santiagos-iMac.local>
2022-06-26 21:31:35 -0300

    Fix cargar una vacuna eliminada con el mismo nombre

--- app/Http/Controllers/VacunaController.php
+++ app/Http/Controllers/VacunaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Vacuna;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class VacunaController extends Controller
 {
@@ -12,37 +13,38 @@ class VacunaController extends Controller
     public function agregarVacuna() {
 
         request()->validate([
-            'nombreVacuna' => 'required | unique:vacunas,nombreVacuna'
+            'nombreVacuna' => 'required'
         ]);
 
-        Vacuna::create([
-            'nombreVacuna' => request()->get('nombreVacuna'),
-            'estado' => 1
-        ]);
+        $existeNombreVacuna = DB::table('vacunas')->where('nombreVacuna', request()->get('nombreVacuna'))->count();
+
+        if($existeNombreVacuna == 0) {  // Si es 0 , el nombreVacuna no esta en la DB
+            Vacuna::create([
+                'nombreVacuna' => request()->get('nombreVacuna'),
+                'estado' => 1
+            ]);
+        }
+        else if($existeNombreVacuna == 1) { // Si es 1 , ya hay una vacuna con ese nombre
+            $estadoVacuna = DB::table('vacunas')->select('estado')->where('nombreVacuna', request()->get('nombreVacuna'))->first();
+
+            if($estadoVacuna->estado == 0) {    // esta cargada y 'eliminada'
+                Vacuna::where('nombreVacuna', request()->get('nombreVacuna'))->update(["estado" => 1]);
+            }
+            else if ($estadoVacuna->estado == 1) { // esta cargada y activa
+                 request()->validate([
+                    'nombreVacuna' => 'unique:vacunas,nombreVacuna'
+                ]);
+            }
+
+        }
 
         return redirect('/vacunas')->with('vacuna-cargada', 'ok');
     }
 
+    // Esto es el 'eliminar' vacuna
     public function edit($id) {
         Vacuna::where("id", $id)->update(["estado" => 0]);
         return redirect('/vacunas')->with('eliminar','ok');
     }
 
-// VER//
-    //public function eliminarVacuna() {
-
-//        request()->validate([
-  //          '*' => 'required'
-    //    ]);
-     //   $nombreVacuna = request()->get('nombreVacuna');
-     //   $nombreVacuna = strtoupper($nombreVacuna);              // cnvierte a mayusculas.
-     //   $vacuna = Vacuna::where("nombreVacuna", $nombreVacuna)->where("nombreVacuna", 'nombreVacuna');
-
-      //  if ($vacuna == 1) {
-      //      Vacuna::update([
-   
-    //            'estado'=> "noDisponible",
-               
-      //      ]);
-    //}
 }
